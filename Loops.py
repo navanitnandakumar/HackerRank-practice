e293f144ad8401a4ff19ceb198edd6b1fc8abc6f
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-13 07:38:42 +0530

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -4,28 +4,3 @@ if __name__ == '__main__':
     while i<n :
         print(i*i)
         i+=1
-        
-#Task
-#Read an integer . For all non-negative integers , print . See the sample for details.
-
-#Input Format
-
-#The first and only line contains the integer, .
-
-#Constraints
-
-
-#Output Format
-
-#Print  lines, one corresponding to each .
-
-#Sample Input 0
-
-#5
-#Sample Output 0
-
-#0
-#1
-#4
-#9
-#16
