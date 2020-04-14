5759e3a2380200c527e3c050cf48494860dadc1a
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-14 11:20:58 +0530

    Create Playing with characters.c

---
+++ Playing with characters.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{   char ch;
+    char s[20];
+    char sen[500];
+    scanf("%c",&ch);
+    scanf("%s",&s);
+    scanf("\n");
+    scanf("%[^\n]%*c", &sen);
+    printf("%c",ch);
+    printf("\n%s",s);
+    printf("\n%s",sen);   
+    return 0;
+}
