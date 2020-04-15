ceb1276b2ee2f46e3c540bfb9dbee9526738970f
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-15 11:41:03 +0530

    Update Basic data types.cpp

--- Basic data types.cpp
+++ Basic data types.cpp
@@ -9,7 +9,7 @@ int main()
     char c;
     float d;
     double e;
-    cin>>a>>b>>c>>d>>e;
-    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<e;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+    printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,d,e);
     return 0;
 }
